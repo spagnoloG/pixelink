5c295e9fd7770efe7f406de93707a9256248fe84
tilenn <tilen.ozbot@gmail.com>
2023-05-19 15:50:20 +0200

    this server

---
+++ rest_api_kp/main_on_server.py
@@ -0,0 +1,252 @@
+from flask import Flask, request, g, jsonify, render_template_string, make_response
+from flask_restful import Resource, Api
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from faker import Faker
+import dicttoxml
+from python_freeipa import Client
+
+app = Flask(__name__)
+api = Api(app)
+
+# freeipa
+app.config["FREEIPA_SERVER"] = "ipa.kp.org"
+app.config["FREEIPA_USERNAME"] = "admin"
+app.config["FREEIPA_PASSWORD"] = "admin12345"
+
+freeipa_client = Client(app.config["FREEIPA_SERVER"])
+freeipa_client.login(app.config["FREEIPA_USERNAME"], app.config["FREEIPA_PASSWORD"])
+
+# Create a MongoDB client, change the << MONGODB URL >> to your MongoDB instance URL
+mongo_url = "mongodb://localhost:27017"
+client = MongoClient(mongo_url)
+
+db = client["my_database"]  # choose your database name
+
+# collections
+users = db["users"]
+messages = db["messages"]
+
+# creating Faker instance for data generation
+fake = Faker()
+
+
+def populate_db():
+    # clear the collections
+    users.delete_many({})
+    messages.delete_many({})
+
+    # generate 10 users and 20 messages
+    for i in range(10):
+        user = {"name": fake.name(), "email": fake.email()}
+        user_result = users.insert_one(user)
+
+        for _ in range(2):
+            message = {
+                "user_id": str(user_result.inserted_id),
+                "content": fake.sentence(),
+            }
+            messages.insert_one(message)
+
+
+class UserAPI(Resource):
+    def get(self, user_id=None):
+        accept = request.headers.get("Accept", "application/json")
+        if accept == "*/*":
+            accept = "application/json"
+        if user_id:
+            user = users.find_one({"_id": ObjectId(user_id)})
+            if user is None:
+                response_data = {"message": "User not found"}
+                if "application/json" in accept:
+                    return response_data, 404
+                elif "application/xml" in accept:
+                    return (
+                        dicttoxml.dicttoxml(response_data).decode(),
+                        404,
+                        {"Content-Type": "application/xml"},
+                    )
+                elif "text/html" in accept:
+                    template = "<html><body><h1>{{ message }}</h1></body></html>"
+                    return (
+                        render_template_string(
+                            template, message=response_data["message"]
+                        ),
+                        404,
+                        {"Content-Type": "text/html"},
+                    )
+                else:
+                    return {"message": "Unsupported Media Type"}, 415
+            user_messages = list(messages.find({"user_id": user_id}))
+            for message in user_messages:
+                message["_id"] = str(message["_id"])
+            user["_id"] = str(user["_id"])
+            user["messages"] = user_messages
+            if "application/json" in accept:
+                return user
+            elif "application/xml" in accept:
+                return (
+                    dicttoxml.dicttoxml(user).decode(),
+                    200,
+                    {"Content-Type": "application/xml"},
+                )
+            elif "text/html" in accept:
+                template = '<html><body><h1>User: {{ user["_id"] }}</h1><p>Messages: {{ user["messages"] }}</p></body></html>'
+                return (
+                    render_template_string(template, user=user),
+                    200,
+                    {"Content-Type": "text/html"},
+                )
+            else:
+                return {"message": "Unsupported Media Type"}, 415
+        else:
+            all_users = list(users.find())
+            for user in all_users:
+                user["_id"] = str(user["_id"])
+                user_messages = list(messages.find({"user_id": str(user["_id"])}))
+                for message in user_messages:
+                    message["_id"] = str(message["_id"])
+                user["messages"] = user_messages
+            if "application/json" in accept:
+                return all_users
+            elif "application/xml" in accept:
+                return (
+                    dicttoxml.dicttoxml(all_users).decode(),
+                    200,
+                    {"Content-Type": "application/xml"},
+                )
+            elif "text/html" in accept:
+                template = (
+                    "<html><body><h1>All Users</h1><p>{{ users }}</p></body></html>"
+                )
+                return (
+                    render_template_string(template, users=all_users),
+                    200,
+                    {"Content-Type": "text/html"},
+                )
+        return {"message": "Unsupported Media Type"}, 415
+
+    def post(self):
+        user = request.get_json()
+        result = users.insert_one(user)
+        return {"_id": str(result.inserted_id)}
+
+    def put(self, user_id):
+        user = request.get_json()
+        result = users.update_one({"_id": ObjectId(user_id)}, {"$set": user})
+        return {"modified_count": result.modified_count}
+
+    def delete(self, user_id):
+        accept = request.headers.get("Accept", "application/json")
+        if accept == "*/*":
+            accept = "application/json"
+        result = users.delete_one({"_id": ObjectId(user_id)})
+        response_data = {"message": "User not found"}
+        if result.deleted_count == 0:
+            if "application/xml" in accept:
+                return (
+                    dicttoxml.dicttoxml(response_data).decode(),
+                    200,
+                    {"Content-Type": "application/xml"},
+                )
+            elif "text/html" in accept:
+                template = "<html><body><h1>User not found</h1></body></html>"
+                return template, 200, {"Content-Type": "text/html"}
+            else:
+                # default to json
+                return response_data, 404
+
+        response_data = {"message": "User deleted"}
+        if "application/xml" in accept:
+            return (
+                dicttoxml.dicttoxml(response_data).decode(),
+                200,
+                {"Content-Type": "application/xml"},
+            )
+        elif "text/html" in accept:
+            template = "<html><body><h1>User deleted</h1></body></html>"
+            return template, 200, {"Content-Type": "text/html"}
+        else:
+            # default to json
+            return response_data, 200
+
+
+class MessageAPI(Resource):
+    def get(self, message_id=None):
+        print(request.cookies)
+        print(g)
+        if "ipa_session" not in request.cookies and not "user" in g:
+            response_data = {"message": "Not logged in"}
+            return response_data, 401
+
+        accept = request.headers.get("Accept", "application/json")
+        if accept == "*/*":
+            accept = "application/json"
+
+        if message_id:
+            message = messages.find_one({"_id": ObjectId(message_id)})
+            if message is None:
+                response_data = {"message": "Message not found"}
+                return self.render_response(response_data, accept, 404)
+            message["_id"] = str(message["_id"])
+            return self.render_response(message, accept)
+
+        all_messages = list(messages.find())
+        for message in all_messages:
+            message["_id"] = str(message["_id"])
+        return self.render_response(all_messages, accept)
+
+    def render_response(self, data, accept, status_code=200):
+        if "application/json" in accept:
+            return data, status_code
+        elif "application/xml" in accept:
+            xml = dicttoxml.dicttoxml(data).decode()
+            return xml, status_code, {"Content-Type": "application/xml"}
+        elif "text/html" in accept:
+            template = "<html><body><h1>{{ data }}</h1></body></html>"
+            html = render_template_string(template, data=data)
+            return html, status_code, {"Content-Type": "text/html"}
+        else:
+            return {"message": "Unsupported Media Type"}, 415
+
+    def post(self):
+        message = request.get_json()
+        result = messages.insert_one(message)
+        return {"_id": str(result.inserted_id)}
+
+    def put(self, message_id):
+        message = request.get_json()
+        result = messages.update_one({"_id": ObjectId(message_id)}, {"$set": message})
+        return {"modified_count": result.modified_count}
+
+    def delete(self, message_id):
+        result = messages.delete_one({"_id": ObjectId(message_id)})
+        if result.deleted_count == 0:
+            return {"message": "Message not found"}, 404
+        return {"deleted_count": result.deleted_count}
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    try:
+        freeipa_client.login(username, password)
+        g.user = username
+        response = make_response("Logged in successfully")
+        cookies = freeipa_client._session.cookies.get_dict()
+        for name, value in cookies.items():
+            response.set_cookie(name, value)
+        return response
+    except Exception as e:
+        # Authentication failed
+        return "Authentication failed: {}".format(str(e))
+
+
+api.add_resource(UserAPI, "/users/<user_id>", "/users")
+api.add_resource(MessageAPI, "/messages/<message_id>", "/messages")
+
+if __name__ == "__main__":
+    populate_db()
+    app.run(debug=True)
